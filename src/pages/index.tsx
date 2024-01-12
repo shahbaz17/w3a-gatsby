c086ec45d12606d62827e24b5830401c27d93211
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2024-01-12 09:38:07 +0530

    nit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -108,44 +108,42 @@ const IndexPage: React.FC<PageProps> = () => {
   };
 
   return (
-    <main style={pageStyles}>
-      {loggedIn ? (
-        <>
-          <button style={buttonStyle} onClick={() => getUserInfo()}>
-            Get User Info
-          </button>
-          {userInfo && (
-            <div>
-              <h2>User Info:</h2>
-              <pre>{JSON.stringify(userInfo, null, 2)}</pre>
-            </div>
-          )}
+    <>
+      <h2> Web3Auth x Gatsby Demo</h2>
+      <main style={pageStyles}>
+        {loggedIn ? (
+          <>
+            <button style={buttonStyle} onClick={() => getUserInfo()}>
+              Get User Info
+            </button>
+            {userInfo && (
+              <div>
+                <h2>User Info:</h2>
+                <pre>{JSON.stringify(userInfo, null, 2)}</pre>
+              </div>
+            )}
 
-          <button style={buttonStyle} onClick={() => getPrivateKey()}>
-            Get Pk
-          </button>
-          {privateKey && (
-            <div>
-              <h2>Private Key:</h2>
-              <pre>{privateKey}</pre>
-            </div>
-          )}
+            <button style={buttonStyle} onClick={() => getPrivateKey()}>
+              Get Pk
+            </button>
+            {privateKey && (
+              <div>
+                <h2>Private Key:</h2>
+                <pre>{privateKey}</pre>
+              </div>
+            )}
 
-          <button style={buttonStyle} onClick={() => signOut()}>
-            Sign Out
+            <button style={buttonStyle} onClick={() => signOut()}>
+              Sign Out
+            </button>
+          </>
+        ) : (
+          <button style={buttonStyle} onClick={() => signIn()}>
+            Sign In
           </button>
-        </>
-      ) : (
-        <button style={buttonStyle} onClick={() => signIn()}>
-          Sign In
-        </button>
-      )}
-
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+        )}
+      </main>
+    </>
   );
 };
 
